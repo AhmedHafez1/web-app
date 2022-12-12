e29e40c977350e4e040ebb306bd53c562cc05867
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-12-12 06:51:37 +0200

    Added get, on and trigger to user class

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,17 +3,17 @@ type Callback = () => void;
 export class Eventing {
   private events: { [key: string]: Callback[] } = {};
 
-  on(eventName: string, callback: Callback) {
+  on = (eventName: string, callback: Callback) => {
     const eventHandlers = this.events[eventName] || [];
     eventHandlers.push(callback);
     this.events[eventName] = eventHandlers;
-  }
+  };
 
-  trigger(eventName: string) {
+  trigger = (eventName: string) => {
     const eventHandlers = this.events[eventName];
 
     if (!eventHandlers?.length) return;
 
     eventHandlers.forEach((callback) => callback());
-  }
+  };
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Http as Http } from './Http';
 
@@ -11,4 +12,21 @@ const userUrl = 'http://localhost:3000/users';
 export class User {
   public eventing = new Eventing();
   public http = new Http<UserData>(userUrl);
+  public attributes: Attributes<UserData>;
+
+  constructor(data: UserData) {
+    this.attributes = new Attributes<UserData>(data);
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  get on() {
+    return this.eventing.on;
+  }
+
+  get trigger() {
+    return this.eventing.trigger;
+  }
 }
